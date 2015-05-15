abb1f57db6f3f2f1d7d693b5e4bcb60a3d46c3ad
asdqwex <matt.ellsworth@matthewellsworth.com>
2015-05-15 00:21:01 -0500

    Fixes #4

--- v2.rb
+++ v2.rb
@@ -56,3 +56,44 @@ end
 puts 'Find the sum of all even numbers in the Fibonacci sequence below '+a.to_s
 puts fibSumsBelow(a)
 puts '##########################################################'
+
+puts '##########################################################'
+puts 'euler #3 - A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.'
+puts 'Find the largest palindrome made from the product of two 3-digit numbers.'
+puts '##########################################################'
+puts 'Find the largest palindrome made from the product of two x-digit numbers.'
+puts '##########################################################'
+puts 'enter a number for x:'
+a = gets.to_i
+puts '##########################################################'
+
+def findLargePal (n)
+    lower = []
+    upper = []
+
+    1.upto(n) do |n|
+        if lower.length < 1
+            lower.push(1)
+        else
+            lower.push(0)
+        end 
+        upper.push(9) 
+    end
+
+    result = 0
+
+    (lower.join.to_i..upper.join.to_i).each do |x|
+        (lower.join.to_i..upper.join.to_i).each do |y|
+            out = x * y 
+            rev = out.to_s.reverse.to_i
+            if out == rev && out > result
+                result = out
+            end
+        end
+    end
+    puts result
+end
+
+puts 'Find the largest palindrome made from the product of two x-digit numbers. Where x is: '+a.to_s
+puts findLargePal(a)
+puts '##########################################################'
