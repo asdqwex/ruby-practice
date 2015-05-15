0fbcc7c3704329b464e47aa8c51cf2d7d54ff085
asdqwex <matt.ellsworth@matthewellsworth.com>
2015-05-15 01:03:44 -0500

    added #3 and added blank rails setup for app demo of euler problems

--- v2.rb
+++ v2.rb
@@ -58,7 +58,27 @@ puts fibSumsBelow(a)
 puts '##########################################################'
 
 puts '##########################################################'
-puts 'euler #3 - A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.'
+puts 'euler #3 - The prime factors of 13195 are 5, 7, 13 and 29.'
+puts 'What is the largest prime factor of the number 600851475143 ?'
+puts '##########################################################'
+puts 'What is the largest prime factor of x'
+puts '##########################################################'
+puts 'enter a number for x:'
+a = gets.to_i
+puts '##########################################################'
+
+require 'prime'
+
+def primeFactors (n)
+    return Prime.prime_division(n) 
+end
+
+puts 'The largest prime factor of x is: '+a.to_s
+puts primeFactors(a)
+puts '##########################################################'
+
+puts '##########################################################'
+puts 'euler #4 - A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.'
 puts 'Find the largest palindrome made from the product of two 3-digit numbers.'
 puts '##########################################################'
 puts 'Find the largest palindrome made from the product of two x-digit numbers.'
