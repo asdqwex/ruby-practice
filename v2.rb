8ca4174acc15e194e84d6c79e06346530cbc9c37
Matt Ellsworth <matt5546@mj91j5f4jl.rackspace.corp>
2015-05-14 22:56:54 -0500

    Fixes #1

--- v2.rb
+++ v2.rb
@@ -1,3 +1,4 @@
+puts '##########################################################'
 puts 'euler #1 -  If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.'
 puts 'Find the sum of all the multiples of 3 or 5 below 1000.'
 puts '##########################################################'
