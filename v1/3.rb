e9f2dd7cee38a1b3a3728268990264f26bc14cc8
asdqwex <matthew.ellsworth@matthewellsworth.com>
2015-06-03 22:47:41 -0500

    formated output for 3

--- v1/3.rb
+++ v1/3.rb
@@ -1,5 +1,6 @@
 ####3. The prime factors of 13195 are 5, 7, 13 and 29.
 ####What is the largest prime factor of the number 600851475143 ?
+require 'prime'
 
-    out = Prime.prime_division(600851475143) 
-    puts out
+out = Prime.prime_division(600851475143)
+puts out.max[0]
