e17373795c9dccf30b038bcfcd7f56580e77543d
Matthew Ellsworth <matthew.ellsworth@matthewellsworth.com>
2016-04-18 23:10:10 -0600

    fixed sum

--- sum.rb
+++ sum.rb
@@ -2,10 +2,10 @@ data = {}
 
 File.open('data.in').each do |line|
 	parts = line.tr('"', '').split(',')
-	if data.key?(parts[1])
-		data[parts[2]] = data[parts[2]] + parts[1]
+	if data.key?(parts[2])
+		data[parts[2]] = data[parts[2]] + parts[1].to_i
 	else
-		data[parts[2]] = parts[1]
+		data[parts[2]] = parts[1].to_i
 	end
 end
 
