c7b1d2e73a0b615f5fcb610402dd8c478490aa97
Matthew Ellsworth <matthew.ellsworth@clickbank.com>
2016-04-20 15:11:57 -0600

    more explicit naming

--- misc/logparse_module.rb
+++ misc/logparse_module.rb
@@ -1,12 +1,12 @@
 #!/usr/bin/env ruby
 class Logparse
-	def count_actors_actions(def_actor, def_action, data_source)
+	def count_actors_actions(actor_regex, action_regex, data_source)
 		data = {}
-		File.foreach(data_source)  do |line|	
-			actor = line.scan(def_actor)
-			action = line.scan(def_action)
+		File.foreach(data_source) do |line|	
+			actor = line.scan(actor_regex)
+			action = line.scan(action_regex)
 			data[actor] = {} unless data.has_key? actor
-			data[actor][action] = 1 unless data[actor][action]
+			data[actor][action] = 0 unless data[actor][action]
 			data[actor][action] = data[actor][action] + 1
 		end
 		return data
