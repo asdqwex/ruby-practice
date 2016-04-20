48b593830aab8dfa24d37ad16ff0367c92afc1d8
Matthew Ellsworth <matthew.ellsworth@clickbank.com>
2016-04-20 09:24:08 -0600

    modularization

--- misc/logparse.rb
+++ misc/logparse.rb
@@ -1,11 +1,15 @@
-data = {}
-
-File.foreach('log.file')  do |line|	
-	ip_address = line.scan(/\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b/)
-	verb = line.scan(/GET|HEAD|POST|PUT|DELETE|TRACE|CONNECT/)
-	data[ip_address] = {} unless data.has_key? ip_address
-	data[ip_address][verb] = 1 unless data[ip_address][verb]
-	data[ip_address][verb] = data[ip_address][verb] + 1
+#!/usr/bin/env ruby
+def count_actors_actions(def_actor, def_action, data_source)
+	data = {}
+	File.foreach(data_source)  do |line|	
+		actor = line.scan(def_actor)
+		action = line.scan(def_action)
+		data[actor] = {} unless data.has_key? actor
+		data[actor][action] = 1 unless data[actor][action]
+		data[actor][action] = data[actor][action] + 1
+	end
+	return data
 end
-
-puts data
+match_ip = /\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b/
+match_http_verb = /GET|HEAD|POST|PUT|DELETE|TRACE|CONNECT/
+puts count_actors_actions(match_ip, match_http_verb, 'log.file' )
