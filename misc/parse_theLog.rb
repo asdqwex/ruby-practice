2e4bf71ba27c069ced0989220ca2be3a8a104ee7
Matthew Ellsworth <matthew.ellsworth@clickbank.com>
2016-04-20 09:43:39 -0600

    as a class

--- misc/parse_theLog.rb
+++ misc/parse_theLog.rb
@@ -2,6 +2,8 @@
 
 require './logparse_module.rb'
 
+parser = Logparse.new()
+
 match_ip = /\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b/
 match_http_verb = /GET|HEAD|POST|PUT|DELETE|TRACE|CONNECT/
-puts Logparse.new.count_actors_actions(match_ip, match_http_verb, 'log.file' )
+puts parser.count_actors_actions(match_ip, match_http_verb, 'log.file' )
