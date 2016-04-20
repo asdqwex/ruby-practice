d9f612f481304bdbef1b69c01b91b7494144b8cd
Matthew Ellsworth <matthew.ellsworth@matthewellsworth.com>
2016-04-19 21:14:16 -0600

    way better

--- misc/logparse.rb
+++ misc/logparse.rb
@@ -1,22 +1,11 @@
 data = {}
 
-File.open('log.file').each do |line|
+File.foreach('log.file')  do |line|	
 	ip_address = line.scan(/\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b/)
-	# puts ip_address
 	verb = line.scan(/GET|HEAD|POST|PUT|DELETE|TRACE|CONNECT/)
-	# puts verb
-	# puts data
-	if data.key?(ip_address) && data[ip_address].key?(verb)
-		# puts 'incrementing'
-		data[ip_address][verb] = data[ip_address][verb] + 1
-	else
-		# puts 'setting'
-		if data[ip_address].class == Hash
-			data[ip_address][verb] = 1
-		else
-			data[ip_address] = {verb=> 1}
-		end
-	end
+	data[ip_address] = {} unless data.has_key? ip_address
+	data[ip_address][verb] = 1 unless data[ip_address][verb]
+	data[ip_address][verb] = data[ip_address][verb] + 1
 end
 
 puts data
